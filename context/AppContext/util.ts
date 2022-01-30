2024467220b4c0e0be504b61635bc0f5d65a01b2
karnell schultz <50870173+KarnellSchultz@users.noreply.github.com>
2022-01-30 13:44:27 +0100

    fix: util styles

--- context/AppContext/util.ts
+++ context/AppContext/util.ts
@@ -1,13 +1,10 @@
-import { WordListData } from "wordData";
-import { WordDataType } from "./types";
+import { WordListData } from 'wordData'
+import { WordDataType } from './types'
 
 export const getRandomNumber = (): number =>
-  Math.floor(Math.random() * WordListData.length - 10);
+    Math.floor(Math.random() * WordListData.length - 10)
 
 export const getWordSlice = (): WordDataType[] => {
-  const sliceStart = getRandomNumber();
-  const CreateNewSlice = () => WordListData.slice(sliceStart, sliceStart + 10);
-  const result = CreateNewSlice();
-
-  return result;
-};
+    const sliceStart = getRandomNumber()
+    return WordListData.slice(sliceStart, sliceStart + 10)
+}
