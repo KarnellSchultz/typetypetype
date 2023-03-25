044aa1e89b567522a68d1d2279eb075dee83cf8a
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2023-03-25 09:59:10 +0100

    update page and next0env

--- app/page.tsx
+++ app/page.tsx
@@ -8,30 +8,30 @@ import React, { useState } from 'react'
 // type HomeProps = { testWords: TestWordType }
 
 const Home = () => {
-    const testWords = useWordList()
+    const wordList = useWordList()
 
     const [inputValue, setInputValue] = useState('')
 
-    const firstTestWordSlice = testWords.slice(0, 10)
-    const secondTestWordSlice = testWords.slice(10, 20)
+    const firstWordSlice = wordList.slice(0, 10)
+    const secondWordSlice = wordList.slice(10, 20)
 
     return (
-        <div>
+        <div className=''>
             <div className="flex">
-                {firstTestWordSlice.map((wordObj) => {
+                {firstWordSlice.map((item) => {
                     return (
-                        <div className="p-1" key={wordObj.id}>
-                            {wordObj.word}
-                        </div>
+                        <span className="p-1" key={item.id}>
+                            {item.word}
+                        </span>
                     )
                 })}
             </div>
             <div className="flex">
-                {secondTestWordSlice.map((wordObj) => {
+                {secondWordSlice.map((item) => {
                     return (
-                        <div className="p-1" key={wordObj.id}>
-                            {wordObj.word}
-                        </div>
+                        <span className="p-1" key={item.id}>
+                            {item.word}
+                        </span>
                     )
                 })}
             </div>
@@ -48,6 +48,31 @@ const Home = () => {
                     name="test-input"
                     id="input"
                 />
+            </form>
+
+
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                        Username
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                </div>
+                <div className="mb-6">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                        Password
+                    </label>
+                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+                    <p className="text-red-500 text-xs italic">Please choose a password.</p>
+                </div>
+                <div className="flex items-center justify-between">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                        Sign In
+                    </button>
+                    <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                        Forgot Password?
+                    </a>
+                </div>
             </form>
             <TestBar
                 // Add func for starting the app
