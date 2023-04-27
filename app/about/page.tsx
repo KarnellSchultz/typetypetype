bdde29eeaf6a38af3cd299d937c198e78d22b261
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2023-04-27 08:31:50 +0200

    smol updates

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,13 @@
 const AboutPage = () => {
     return (
-        <div>
+        <div className="text-center">
             <h1 className="text-5xl font-bold">About</h1>
+            <p>The site is a work in progress, so you can expect bugs and missing features everywhere.</p>
+            <br />
             <p className="text-xl">
-                Typetypetype is a typing game that helps you learn new words.
+                Typetypetype is a typing speed game.
+                The goal is to type as many words as possible in a given time.
+                The game is over when the time runs out.
             </p>
             <p className="text-xl">
                 woot
